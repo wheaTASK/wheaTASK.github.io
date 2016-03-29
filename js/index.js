b738147323e58819b84d91ecc113a51eb819d240
Tyler Kickham <tkicks32@gmail.com>
2016-03-28 21:38:40 -0400

    some updates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,32 @@
 // contains js for index page
 $(document).ready(function() {
-	// js and jquery
+	// submenu
+	$("#submenu").hide();
+	$(".submenuOpen").click(function() {
+		$("#submenu").slideDown();
+		$(".submenuOpen").hide();
+	});
+	$(".submenuEnter").click(function() {
+		checkboxes();
+	});
+	$(".submenuClose").click(function() {
+		$("#submenu").slideUp("slow", function() {
+			$(".submenuOpen").show();
+		});
+		checkboxes();
+	});
 })
+
+// checkboxes
+function checkboxes() {
+	if ($("#selectAll").is(':checked')) {
+		$('input[type=checkbox]').each(function() {
+			this.checked = true;
+		});
+	}
+	else if ($("#selectAll").not(':checked')) {
+		$('input[type=checkbox]').each(function() {
+			this.checked = false;
+		});
+	}
+}
