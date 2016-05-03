f7ded5505f7f906c15539a3e368edb37ee683b12
Tyler Kickham <tkicks32@gmail.com>
2016-05-02 20:18:20 -0400

    can't go back past 4/22

--- js/datepicker.js
+++ js/datepicker.js
@@ -5,6 +5,9 @@ var endDay;
 var startYear;
 var startMonth;
 var startDay;
+var farthestBackMonth = 4;
+var farthestBackDay = 22;
+var farthestBackYear = 2016;
 
 
 $(document).ready(function(){
@@ -12,9 +15,12 @@ $(document).ready(function(){
   	var today = new Date();
 
   	// get the end date (today) and start date (default to 1 month before)
-  	endYear = today.getFullYear();
-	endMonth = today.getMonth()+1;
-	endDay = today.getDate();
+  	// endYear = today.getFullYear();
+	// endMonth = today.getMonth()+1;
+	// endDay = today.getDate();
+	endYear = 2016;
+	endMonth = 5;
+	endDay = 1;
 	startYear = endYear;
 	startMonth = endMonth - 1;
 	startDay = endDay;
@@ -41,27 +47,37 @@ $(document).ready(function(){
 
 	$('#dp4').fdatepicker()
 		.on('changeDate', function (ev) {
-		if (ev.date.valueOf() > endDate.valueOf()) {
-			$('#alert').show().find('strong').text('The start date can not be greater then the end date');
-		} else {
-			$('#alert').hide();
-			startDate = new Date(ev.date);
-			$('#startDate').text($('#dp4').data('date'));
-			changeStartDate(startDate);
+		var newDate = new Date(ev.date);
+		var validDate = checkDate(newDate);
+		if (validDate) {
+			if (ev.date.valueOf() > endDate.valueOf()) {
+				$('#alert').show().find('strong').text('The start date can not be greater then the end date');
+			} else {
+				$('#alert').hide();
+				startDate = new Date(ev.date);
+				$('#startDate').text($('#dp4').data('date'));
+				changeStartDate(startDate);
+				var newEndDate = fixEndDate(startDate);
+				changeEndDate(newEndDate);
+			}
+			$('#dp4').fdatepicker('hide');
 		}
-		$('#dp4').fdatepicker('hide');
 	});
 	$('#dp5').fdatepicker()
 		.on('changeDate', function (ev) {
-		if (ev.date.valueOf() < startDate.valueOf()) {
-			$('#alert').show().find('strong').text('The end date can not be less then the start date');
-		} else {
-			$('#alert').hide();
-			endDate = new Date(changeEndDate(ev.date));
-			$('#endDate').text($('#dp5').data('date'));
-			// changeEndDate(endDate);
+		var newDate = new Date(ev.date);
+		var validDate = checkDate(newDate);
+		if (validDate) {
+			if (ev.date.valueOf() < startDate.valueOf()) {
+				$('#alert').show().find('strong').text('The end date can not be less then the start date');
+			} else {
+				$('#alert').hide();
+				endDate = new Date(changeEndDate(ev.date));
+				$('#endDate').text($('#dp5').data('date'));
+				// changeEndDate(endDate);
+			}
+			$('#dp5').fdatepicker('hide');
 		}
-		$('#dp5').fdatepicker('hide');
 	});
 });
 
@@ -85,4 +101,46 @@ function changeEndDate(endDate) {
 	endMonth = checkFormat(endDate.getMonth()+1);
 	endDay = checkFormat(endDate.getDate());
 	// checkFormat(startMonth);
+}
+
+function checkDate(newDate) {
+	var newDay = newDate.getDate();
+	var newMonth = newDate.getMonth()+1;
+	var newYear = newDate.getFullYear();
+
+	if (newDay == 31 && newMonth == 3)
+		newDay = 1;
+	else if (newDay == 30 && newMonth == 4)
+		newDay = 1;
+	else
+		newDay += 1;
+
+	// for some reason first of month gets month number of previous month
+	console.log(newDay);
+	if (newDay == 1) {
+		console.log("chose first");
+		newMonth += 1;
+	}
+
+	// console.log(newMonth);
+
+	if (newYear !== 2016) {
+		console.log("invalid year");
+		return false;
+	}
+	if (newMonth < 4 || newMonth > 5) {
+		console.log("invalid month");
+		return false;
+	}
+	if (newMonth == 4 && newDay < farthestBackDay) {
+		console.log("invalid day");
+		return false;
+	}
+	return true;
+}
+
+function fixEndDate(startDate) {
+	var newDay = newDate.getDate();
+	var newMonth = newDate.getMonth()+1;
+	var newYear = newDate.getFullYear();
 }
